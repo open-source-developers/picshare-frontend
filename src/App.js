59a9b8049e6801e448079b7be7556991f6841654
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-01-23 15:13:30 -0500

    Added navbar

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Feed from './components/Feed/Feed';
 import Settings from './components/Settings/Settings';
+import Navbar from './components/shared/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/profile" exact component={Profile} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/register" exact component={Register} />
-            <Route path="/feed" exact component={Feed} />
-            <Route path="/settings" exact component={Settings} />
-          </Switch>
+          <div>
+            <Navbar />
+            <Switch>
+              {/* <Route path="/" exact component={LandingPage} /> */}
+              <Route path="/profile" exact component={Profile} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/register" exact component={Register} />
+              <Route path="/settings" exact component={Settings} />
+              <Route path="/(|feed)" exact component={Feed} />
+            </Switch>
+          </div>
         </Router>
       </div>
     );
